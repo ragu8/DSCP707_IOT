c6a3c4a1e79a3da0b0ce834a5e070d4efa097420
ragu8 <ragumdr2023@gmail.com>
2022-11-20 10:17:25 +0530

    init -2

--- MOTION DETECTION USING ARDUINO/MOTION/MOTION.ino
+++ MOTION DETECTION USING ARDUINO/MOTION/MOTION.ino
@@ -1,44 +1,20 @@
-#include <Wire.h>
-const int buzzer = 11;
-const int light = 13;
-int sensor = 2;// the pin that the sensor is atteched to
-int state = LOW;// by default, no motion detected
-int val = 0;
-// variable to store the sensor status (value)
-void setup() {
-Serial.begin(9600);
-pinMode(buzzer, OUTPUT);
-pinMode(light, OUTPUT);
-pinMode(sensor, INPUT); // initialize sensor as an input
-Serial.println("HELLO RAGU "); // print some text in Serial Monitor
-Serial.println("Home Automation Program");
-}
-void loop() {
-val = digitalRead(sensor); // read sensor value
-if (val == HIGH) {
-delay(100);
-// check if the sensor is HIGH
-// delay 100 milliseconds
-if (state == LOW) {
-Serial.println("Motion Detected");
-Serial.println("");
-digitalWrite(light, HIGH);
-tone(buzzer, 1000); // Send 1KHz sound signal...
-delay(500);
-// ...for 1 sec
-tone(buzzer, 500);
-noTone(buzzer);// Stop sound...
-state = HIGH;// update variable state to HIGH
-}
-}
-else {
-delay(100);
-// delay 200 milliseconds
-if (state == HIGH){
-Serial.println("Motion stopped!");
-digitalWrite(light, LOW);
-state = LOW;
-// update variable state to LOW
-}
-}
+int led = 13;
+int sensor = A3;void setup() {
+  pinMode(led, OUTPUT);
+  pinMode(sensor, INPUT);
+  Serial.begin(9600);
+}void loop() {
+  int sensorval = analogRead(sensor);
+  Serial.println(sensorval);
+  
+  if (sensorval > 234) {
+    digitalWrite(led, HIGH);
+    Serial.println(" Some Motion");
+    Serial.println(sensorval);
+  }
+  else {
+   digitalWrite(led, LOW);
+   Serial.println("NO Motion");
+   Serial.println(sensorval);
+  }
 }
