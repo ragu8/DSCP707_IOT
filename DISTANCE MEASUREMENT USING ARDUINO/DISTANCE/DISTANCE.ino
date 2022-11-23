da6bf1cbc5ee8e8bf7aab23228d97706afba1562
ragu8 <ragumdr2023@gmail.com>
2022-11-23 12:20:21 +0530

    update

--- DISTANCE MEASUREMENT USING ARDUINO/DISTANCE/DISTANCE.ino
+++ DISTANCE MEASUREMENT USING ARDUINO/DISTANCE/DISTANCE.ino
@@ -3,18 +3,15 @@
 #define trigPin 2
 long duration;
 int distance;
-const int buzzer = 8;
-const int light = 13;
-const int light2 = 12;
+
 void setup() {
 pinMode(trigPin, OUTPUT);
 pinMode(echoPin, INPUT);
 Serial.begin(9600);
-pinMode(buzzer, OUTPUT);
-pinMode(light, OUTPUT);
-Serial.println("HELLO RAGU "); // print some text in Serial Monitor
+Serial.println("HELLO RAGU "); 
 Serial.println("Distance Measure Program");
 }
+
 void loop() {
 digitalWrite(trigPin, LOW);
 delayMicroseconds(2);
@@ -26,26 +23,16 @@ distance = duration * 0.034 / 2;
 Serial.print("Distance: ");
 Serial.print(distance);
 Serial.println(" cm");
-digitalWrite(light, LOW);
-digitalWrite(light2, LOW);
+
 if (distance <= 10)
 {
 Serial.println("Very Close");
-digitalWrite(light, HIGH);
-tone(buzzer, 1000); // Send 1KHz sound signal...
 delay(1000);
-// ...for 1 sec
-digitalWrite(light2, LOW);
-tone(buzzer, 500);
-noTone(buzzer);
-// Stop sound...
 }
 else if (distance >=11 && distance <=50)
 {
 Serial.println("Near Range");
-digitalWrite(light2, HIGH);
-digitalWrite(light, LOW);
-delay(1000);
+delay(10000);
 }
 else
 {
--- MOTION DETECTION USING ARDUINO/MOTION/MOTION.ino
+++ MOTION DETECTION USING ARDUINO/MOTION/MOTION.ino
@@ -1,6 +1,6 @@
-int led = 13;
+
 int sensor = A3;void setup() {
-  pinMode(led, OUTPUT);
+
   pinMode(sensor, INPUT);
   Serial.begin(9600);
 }void loop() {
@@ -8,12 +8,12 @@ int sensor = A3;void setup() {
   Serial.println(sensorval);
   
   if (sensorval > 234) {
-    digitalWrite(led, HIGH);
+
     Serial.println(" Some Motion");
     Serial.println(sensorval);
   }
   else {
-   digitalWrite(led, LOW);
+ 
    Serial.println("NO Motion");
    Serial.println(sensorval);
   }
